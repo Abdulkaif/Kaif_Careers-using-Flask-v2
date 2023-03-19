b0a3955e2846e4022e6fadfaf2ceb51a08d6386f
Abdul Kaif <>
2023-03-19 13:11:13 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+JOBS = [
+  {
+    "job": "Data Engineer",
+    "location": "Hyderabad",
+    "salary": 10000,
+  },
+  {
+    "job": "Data Scientist",
+    "location": "Banglore",
+    "salary": 18000,
+  },
+  {
+    "job": "Backend Engineer",
+    "location": "Pune",
+    "salary": 20000,
+  },
+  {
+    "job": "Frontend Engineer",
+    "location": "Gurgaon",
+  },
+]
+
+
+@app.route('/')
+def index():
+  return render_template("index.html", jobs=JOBS, company_name="Kaif's")
+
+
+app.run(host='0.0.0.0', port=81)
